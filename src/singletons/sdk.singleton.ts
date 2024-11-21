3df48c031ba24ab38d9fe6d2056d7e0d04166ca6
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-11-21 00:26:43 -0300

    fix: sdk singleton

--- src/singletons/sdk.singleton.ts
+++ src/singletons/sdk.singleton.ts
@@ -1,3 +1,8 @@
 import type { SdkOptions } from "../interfaces/mod.ts";
 
-export const SDK: SdkOptions = { host: "127.0.0.1", https: false };
+export const SDK: SdkOptions = {
+  host: "127.0.0.1",
+  https: false,
+  deserializers: [],
+  serializers: [],
+};
