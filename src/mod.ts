eab7165168037776b042bda8a61fc6cdb9cfb0c6
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-12-08 14:39:13 -0300

    export unwrappable

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,7 @@
 export * from "./utils/mod.ts";
 export * from "./types/request-body.type.ts";
 export * from "./types/map-structure.type.ts";
+export * from "./types/unwrappable.type.ts";
 export * from "./classes/mod.ts";
 export * from "./interfaces/content-body.interface.ts";
 export * from "./interfaces/content-response.interface.ts";
