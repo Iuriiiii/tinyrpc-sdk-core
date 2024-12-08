b47a95b9f8d1f8491c41199e11cff6eeb4152116
Iuriiiiiii <alex.casas@webtroniclabs.com>
2024-12-08 14:18:44 -0300

    fmt

--- src/utils/make-it-unwrappable.util.ts
+++ src/utils/make-it-unwrappable.util.ts
@@ -4,9 +4,9 @@ import type { Unwrappable } from "../types/mod.ts";
 export function makeItUnwrappable<
   T extends MethodResponse<unknown>,
   K = T extends MethodResponse<infer U> ? U : never,
->(promisedValue: Promise<T>) {
+>(promisedValue: Promise<T>): Unwrappable<T, K> {
   const result = promisedValue as Unwrappable<T, K>;
   result.unwrap = (async () => (await promisedValue).result) as () => Promise<K>;
 
-  return promisedValue;
+  return result;
 }
